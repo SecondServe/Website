fcfa32da548bc9e0b380dad09645e7548c4822d4
SecondServe <secondservestudios@gmail.com>
2017-08-18 15:01:13 -0600

    Uploaded Index

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<title>
+Second Serve
+</title>
+<header style="background-color:#99ccff;">
+<h2>Second Serve Studios</h2>
+</header>
+
+<nav>
+<ul>
+  <li><a href="/index.php">Home</a></li>
+  <li><a href="/index.php">Contact Us</a></li>
+</ul>
+</nav>
+
+<body style="background-color:grey;">
+This is the body of our website.
+</body>
+
+
+</html>
